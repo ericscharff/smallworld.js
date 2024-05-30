64f1ca314960ef4095a9aac9e2d4de549d6dc9a7
Eric Scharff <eric.scharff@gmail.com>
2024-05-29 18:20:25 -0600

    Fix mistake with object internals
    The object internals missed the concept of MetaClasses, the objects that
    store class methods.

--- object_internals.js
+++ object_internals.js
@@ -74,22 +74,41 @@ ObjectClass.data.push(nilObject);
 
 // For all the objects representing classes, we've not defined their class
 // but have temporarily left it out. The class "String" is an instance of
-// "Class". The class "Class" is an instance of itself. Note that this is
-// different than Smalltalk-80, but works reasonably well.
+// the special class "MetaString". The class object for "String" holds the
+// methods for instances of String. The class object "MetaString" holds all
+// the class methods of String (e.g., String new:). The metaclasses are
+// instances of "Class". "Class" is an instance of "MetaClass", and "MetaClass"
+// is an instance of "Class." This differs somewhat from Smalltalk-80, but
+// works reasonably well.
 const ClassClass = { data: [] };
 const stringClass = { objClass: StringClass, data: [], values: "Class" };
 ClassClass.data.push(stringClass);
 
+const MetaClassClass = { objClass: ClassClass, data: [] };
+const stringMetaClass = {
+  objClass: StringClass,
+  data: [],
+  values: "MetaClass",
+};
+MetaClassClass.data.push(stringMetaClass);
+ClassClass.objClass = MetaClassClass;
+
 // Now, go back and fill out the objClass for all the classes we've defined,
 // so that every object has a class.
-UndefinedClass.objClass = ClassClass;
-StringClass.objClass = ClassClass;
-ObjectClass.objClass = ClassClass;
-ArrayClass.objClass = ClassClass;
-IndexedClass.objClass = ClassClass;
-CollectionClass.objClass = ClassClass;
-MagnitudeClass.objClass = ClassClass;
-ClassClass.objClass = ClassClass;
+const MetaUndefinedClass = { objClass: ClassClass, data: [] };
+UndefinedClass.objClass = MetaUndefinedClass;
+const MetaStringClass = { objClass: ClassClass, data: [] };
+StringClass.objClass = MetaStringClass;
+const MetaObjectClass = { objClass: ClassClass, data: [] };
+ObjectClass.objClass = MetaObjectClass;
+const MetaArrayClass = { objClass: ClassClass, data: [] };
+ArrayClass.objClass = MetaArrayClass;
+const MetaIndexedClass = { objClass: ClassClass, data: [] };
+IndexedClass.objClass = MetaIndexedClass;
+const MetaCollectionClass = { objClass: ClassClass, data: [] };
+CollectionClass.objClass = MetaCollectionClass;
+const MetaMagnitudeClass = { objClass: ClassClass, data: [] };
+MagnitudeClass.objClass = MetaMagnitudeClass;
 
 // The next field of every object is it's methods. Methods is an array of
 // method objects. We can feed in the empty arrays:
