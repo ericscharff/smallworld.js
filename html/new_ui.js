9c1f0d7e7e34bc38da686accb43bb8db49407ab6
Eric Scharff <eric.scharff@gmail.com>
2024-06-08 19:53:31 -0600

    Create a new, HTML-native UI
    This introduces a new Smalltalk class called Element which represents an
    HTML DOM element. It has methods for doing DOM manipulation, such as
    creating elements, finding them in the document, addding and removing CSS
    classes, and appending and removing from the DOM, and handling DOM
    events with Smalltalk blocks.
    
    The end result is a new UIHandler which does basically everything that
    the old UI handler did, but with 10 primitives instead of 17, and a
    great deal of flexibility that the old UI lacked.
    
    This Element handler basically implements the same UI that the old one
    did, but all within the classes (Pane, Menu, Window).
    
    This also demonstrates the power of local variable based closures. The
    list method creates an HTML-style ListBox with only a tiny amount of
    code. This works by creating a click handler on list items that executes
    on the local variable state.
    
    This is not yet integrated with the Web interface, but doing so should
    be relatively straightforward.

---
+++ html/new_ui.js
@@ -0,0 +1,76 @@
+import { SmallByteArray, SmallJsObject } from "../src/objects.js";
+
+export class UIHandler {
+  handle(interpreter, high, stack, stackTop) {
+    let returnedValue = null;
+    switch (high) {
+      case 60: // get element by id
+        const eltIdName = stack[--stackTop].toString();
+        const elt = document.getElementById(eltIdName);
+        if (elt) {
+          returnedValue = new SmallJsObject(stack[--stackTop], elt);
+        } else {
+          returnedValue = interpreter.nilObject;
+        }
+        break;
+      case 61: // create element with tagName
+        const tagName = stack[--stackTop].toString();
+        const newElt = document.createElement(tagName);
+        returnedValue = new SmallJsObject(stack[--stackTop], newElt);
+        break;
+      case 62: // set element innerText
+        const textToSet = stack[--stackTop].toString();
+        const eltForText = stack[--stackTop];
+        eltForText.nativeObject.innerText = textToSet;
+        returnedValue = eltForText;
+        break;
+      case 63: // form element's value
+        const eltForVal = stack[--stackTop];
+        const eltVal = eltForVal.nativeObject.value;
+        // String class is passed in to create the return type
+        const stringClass = stack[--stackTop];
+        returnedValue = new SmallByteArray(stringClass, eltVal);
+        break;
+      case 64: // set form element's value
+        const valToSet = stack[--stackTop].toString();
+        const eltForValSet = stack[--stackTop];
+        eltForValSet.nativeObject.value = valToSet;
+        returnedValue = eltForValSet;
+        break;
+      case 65: // add event listner with block
+        const eventAction = stack[--stackTop];
+        const eventName = stack[--stackTop].toString();
+        const eventTarget = stack[--stackTop];
+        eventTarget.nativeObject.addEventListener(eventName, () => {
+          interpreter.runActionWithValue(eventAction, eventTarget);
+        });
+        returnedValue = eventTarget;
+        break;
+      case 66: // appendChild to element
+        const childElement = stack[--stackTop].nativeObject;
+        const parentStElement = stack[--stackTop];
+        parentStElement.nativeObject.appendChild(childElement);
+        returnedValue = parentStElement;
+        break;
+      case 67: // remove element from DOM
+        stack[--stackTop].nativeObject.remove();
+        returnedValue = interpreter.nilObject;
+        break;
+      case 68: // add class name to classList
+        const clToAdd = stack[--stackTop].toString();
+        const clAddEl = stack[--stackTop];
+        clAddEl.nativeObject.classList.add(clToAdd);
+        returnedValue = clAddEl;
+        break;
+      case 69: // remove class name to classList
+        const clToDel = stack[--stackTop].toString();
+        const clDelEl = stack[--stackTop];
+        clDelEl.nativeObject.classList.remove(clToDel);
+        returnedValue = clDelEl;
+        break;
+      default:
+        throw new Error("Bad UI " + high);
+    }
+    return [returnedValue, stack, stackTop];
+  }
+}
