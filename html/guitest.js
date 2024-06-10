c01d9dbeeea28cc4cb7b7ceb3299545982f7fd97
Eric Scharff <eric.scharff@gmail.com>
2024-06-09 19:09:39 -0600

    Refactor browser test to use SmallWorld

--- html/guitest.js
+++ html/guitest.js
@@ -1,20 +1,9 @@
 import { expect } from "../node_modules/chai/chai.js";
+import { SmallWorld } from "../src/smallworld.js";
 import { UiHandler } from "./new_ui.js";
-import { ImageReader } from "../src/image_reader.js";
-import { Interpreter } from "../src/interpreter.js";
-import { SmallByteArray, SmallObject } from "../src/objects.js";
 
 describe("GUI Test", () => {
-  let nilObject = null;
-  let trueObject = null;
-  let falseObject = null;
-  let smallInts = null;
-  let ArrayClass = null;
-  let BlockClass = null;
-  let ContextClass = null;
-  let IntegerClass = null;
-
-  let interpreter = null;
+  let smallWorld = null;
 
   beforeEach(async () => {
     const ws = document.createElement("div");
@@ -22,73 +11,16 @@ describe("GUI Test", () => {
     document.body.appendChild(ws);
     const resp = await fetch("../data/image-newgui.data");
     const buf = await resp.arrayBuffer();
-    const reader = new ImageReader(buf);
-    nilObject = reader.readObject();
-    trueObject = reader.readObject();
-    falseObject = reader.readObject();
-    smallInts = reader.readSmallInts();
-    ArrayClass = reader.readObject();
-    BlockClass = reader.readObject();
-    ContextClass = reader.readObject();
-    IntegerClass = reader.readObject();
-
-    interpreter = new Interpreter(
-      nilObject,
-      trueObject,
-      falseObject,
-      smallInts,
-      ArrayClass,
-      BlockClass,
-      ContextClass,
-      IntegerClass,
-    );
-    interpreter.uiHandler = new UiHandler();
+    smallWorld = new SmallWorld(buf);
+    smallWorld.interpreter.uiHandler = new UiHandler();
   });
-
-  function runDoIt(task, bytecodePatcher) {
-    // Simulate doIt
-
-    // This relies on the definitions of class
-    // variables: 'name parentClass methods size variables '
-    // and method
-    // variables: 'name byteCodes literals stackSize temporarySize class text '
-    const TrueClass = trueObject.objClass;
-    // the class name (instance var 0) is known to be an instance of String
-    const name = TrueClass.data[0]; // class name (a string)
-    const StringClass = name.objClass;
-    // String class should have a method called "doIt"
-    const methods = StringClass.data[2]; // class methods (an array)
-    // Look for the method
-    let doItMethod = null;
-    for (let i = 0; i < methods.data.length; i++) {
-      const aMethod = methods.data[i];
-      // The method's first instance variable is a SmallByteArray name
-      if (aMethod.data[0].toString() === "doIt") {
-        doItMethod = aMethod;
-        if (bytecodePatcher) {
-          bytecodePatcher(aMethod.data[1].values);
-        }
-      }
-    }
-    if (doItMethod === null) {
-      throw new Error("No doIt method found");
-    } else {
-      // Make the Smalltalk string object on which doIt will be called
-      const taskByteArray = new SmallByteArray(StringClass, 0);
-      taskByteArray.values = new TextEncoder().encode(task);
-      const args = new SmallObject(ArrayClass, 1);
-      args.data[0] = taskByteArray; // This is basically "self" for doIt
-      const ctx = interpreter.buildContext(nilObject, args, doItMethod);
-      return interpreter.execute(ctx);
-    }
-  }
 
   afterEach(() => {
     document.getElementById("workspace").remove();
   });
 
   it("exercises the GUI", () => {
-    runDoIt("Class browser");
+    smallWorld.runDoIt("Class browser");
     function findByText(target, start, eltKind) {
       const xpath = `//${eltKind}[text()='${target}']`;
       return document.evaluate(
