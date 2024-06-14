80540d62e5921a61abcf0483036acab767dfaa3b
Eric Scharff <eric.scharff@gmail.com>
2024-06-13 19:46:13 -0600

    Create a script to make the base (nogui) image
    Previoiusly, I created data/nogui.image by manually editing repl.js and
    pipng remove-gui.st into it.
    
    This new scheme avoids the manual step, but also breaks the image
    generation into more logical chunks (that could potentially be used to
    create different kind of images). The changes in each smaller st source
    file do one specific task.

---
+++ scripts/make_nogui_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Use the base.image to create nogui.image which is headless (removing
+# methods referring to Budd's Java GUI). nogui.image is a good starting point
+# for adding features (such as the HTML based GUI).
+
+cat \
+  add-class-saveimage.st \
+  add-object-log.st \
+  add-trig.st \
+  fix-list-bug.st \
+  redefine-object-error.st \
+  remove-gui.st \
+  save-nogui.st | ./repl.js --image_name ../data/base.image
