a60d63e7a832ca3f8871e53b73afec02a17164fe
Eric Scharff <eric.scharff@gmail.com>
2024-06-13 20:06:46 -0600

    Add shell script to make the HTML GUI image
    Running ./make_newgui_image creates newgui.image from the original Budd
    image. Technically this could be built from nogui.iamge as well

---
+++ scripts/make_newgui_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use the base.image to create newgui.image which replaces Budd's Java GUI
+# with a native HTML based GUI. It introduces the Element class to represent
+# an HTML DOM element.
+
+cat \
+  add-class-saveimage.st \
+  add-object-log.st \
+  add-trig.st \
+  fix-list-bug.st \
+  redefine-object-error.st \
+  remove-gui.st \
+  Element.st \
+  html-editor.st \
+  html-error-handler.st \
+  save-newgui.st | ./repl.js --image_name ../data/base.image
