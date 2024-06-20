1f9b14c91bb5f71a75d405ea07b9764c2cb7f12f
Eric Scharff <eric.scharff@gmail.com>
2024-06-20 07:43:32 -0600

    Create two GUI creation scripts
    It is nice that all of the new images (no GUI and HTML gui) can be
    derived from base, but it's faster to simply regerate the GUI image from
    the nogui image most of the time (though the two scripts create an
    identical image).

--- scripts/make_newgui_from_base_image.sh
+++ scripts/make_newgui_from_base_image.sh
@@ -5,9 +5,18 @@
 # an HTML DOM element.
 
 cat \
+  add-class-saveimage.st \
+  add-object-log.st \
+  add-object-equality.st \
+  add-trig.st \
+  fix-list-bug.st \
+  fix-class-bugs.st \
+  redefine-object-error.st \
+  remove-gui.st \
   Element.st \
   Canvas.st \
   canvas-examples.st \
   html-editor.st \
   html-error-handler.st \
-  save-newgui.st | ./repl.js --image_name ../data/nogui.image
+  recompile.st \
+  save-newgui.st | ./repl.js --image_name ../data/base.image
